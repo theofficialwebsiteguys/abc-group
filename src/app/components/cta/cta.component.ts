be6f4e9e1f3c5532b1d82d102a7402e6b2ffc2fc
Full Name <theofficialwebsiteguys@gmail.com>
2025-06-18 13:23:16 -0400

    final draft

--- src/app/components/cta/cta.component.ts
+++ src/app/components/cta/cta.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cta',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './cta.component.html',
   styleUrl: './cta.component.scss'
 })
--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
