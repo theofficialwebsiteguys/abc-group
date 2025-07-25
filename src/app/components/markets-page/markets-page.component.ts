4f1eb31445c3d1250ce07e7dd22ab3806a2209dc
Full Name <theofficialwebsiteguys@gmail.com>
2025-07-25 15:49:07 -0400

    new updates 7-25

--- src/app/components/markets-page/markets-page.component.ts
+++ src/app/components/markets-page/markets-page.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { CtaComponent } from '../cta/cta.component';
+import { DetailSectionComponent } from '../detail-section/detail-section.component';
+import { ServicesComponent } from '../services/services.component';
+import { TestimonialsComponent } from '../testimonials/testimonials.component';
 
 @Component({
   selector: 'app-markets-page',
-  imports: [],
+  imports: [CtaComponent, DetailSectionComponent, TestimonialsComponent, ServicesComponent],
   templateUrl: './markets-page.component.html',
   styleUrl: './markets-page.component.scss'
 })
 export class MarketsPageComponent {
-
+  testimonial = {
+    quote: "Quality is the best business plan.",
+    name: "John Lasseter",
+    avatar: "assets/john-lasseter.jpeg"
+  };
 }
